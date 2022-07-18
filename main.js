76153b3f0267775d6584f980b294ed4069ebd668
Irina-Dehtiarenko <93826301+Irina-Dehtiarenko@users.noreply.github.com>
2022-07-18 19:10:11 +0200

    added the function of saving the selected mode in localStorage

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 const switchButton = document.querySelector("header button");
 
+let theme = localStorage.getItem("theme") || "light"; //jeśli zczytamy z localstorage team i on nie będzie istniał, to przepiszemy mu wartość light
+
 switchButton.addEventListener("click", () => {
-  document.querySelector("body").classList.toggle("dark");
+  if (theme === "dark") {
+    document.querySelector("body").classList.remove("dark");
+    theme = "light";
+  } else {
+    document.querySelector("body").classList.add("dark");
+    theme = "dark";
+  }
+  localStorage.setItem("theme", theme);
 });
+
+if (theme === "dark") {
+  document.querySelector("body").classList.add("dark");
+}
